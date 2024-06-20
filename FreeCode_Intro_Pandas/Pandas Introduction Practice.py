941f430122ee9be6998b9044a301a86f76f3b986
mitchmeger <Mitchmeger13@gmail.com>
2024-06-19 21:32:08 -0400

    update

--- FreeCode_Intro_Pandas/Pandas Introduction Practice.py
+++ FreeCode_Intro_Pandas/Pandas Introduction Practice.py
@@ -21,5 +21,13 @@ names = ['Tom','Kris', 'Ahmad', 'Beau']
 certificates_earned1.index = names
 longest_streak = pd.Series([13,11,9,7], index = names)
 
-certificates_earned1['longest_streak'] = longest_streak
+certificates_earned1['longest_streak'] = longest_streak ##Adding column longest streak
+print(certificates_earned1)
+
+#pandas is cool 
+
+#make a column called certificates per streak 
+
+cond = round((certificates_earned1["Certificates"])/(certificates_earned1['longest_streak']),2) # certificates/ longest streak
+certificates_earned1["Cert per Streak"]= cond 
 print(certificates_earned1)
